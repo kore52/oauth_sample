1cbc1dee524fe3650b200a679fee0271d8ac2cbf
kore52 <lm312v21@gmail.com>
2016-09-20 14:48:55 +0900

    test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var session = require('express-session');
+var partials = require('express-partials');
 
 // ページ遷移
 var routes = require('./routes/index');
@@ -27,6 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 app.use(session({
   secret: 'the quick brown fox',
   resave: false,
