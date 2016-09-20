4d11badac5a4db4870be34f8831cbea8a69a8e8e
kore52 <lm312v21@gmail.com>
2016-09-20 10:47:17 +0900

    test

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   if (req.isAuthenticated()) {
-    res.render('users', { title: 'CTF Dashboard', username: req.user.username, profile: "<pre>" + JSON.stringify(req.user, null, 4) + "</pre>" });
+    res.render('users', { title: 'CTF Dashboard', username: req.user.username, profile: JSON.stringify(req.user, null, 4) });
   } else {
     res.redirect('../');
   }
