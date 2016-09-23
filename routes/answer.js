22f6e06608a75eca3d27a7eca59516c9b8641bb8
kore52 <lm312v21@gmail.com>
2016-09-23 15:18:20 +0900

    test

---
+++ routes/answer.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+
+// MongoDBèâä˙âª
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+/*
+var UserSchema = new Schema({
+  provider: { type: String, required: true },
+  provider_id: { type: String, required: true },
+  nickname: { type: String },
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now }
+});
+*/
+var ScoreSchema = new Schema({
+  user_id: { type: String, required: true },
+  problem_id: { type: String, required: true },
+  score: { type: Number }
+});
+
+var mongodb_uri = process.env.MONGODB_URI || '';
+//mongoose.model('User', UserSchema);
+mongoose.model('Score', ScoreSchema);
+mongoose.Promise = global.Promise;
+mongoose.connect(mongodb_uri);
+var User = mongoose.model('User');
+var Score = mongoose.model('Score');
+
+router.get('/', function(req, res, next) {
+  if (req.isAuthenticated()) {
+    var answer = {
+      "1" : {
+        answer : "CTFTUTORIAL",
+        score : 30
+      }
+    };
+    
+    try {
+      var post_id = req.body.problem_id;
+      var post_answer = req.body.answer;
+      
+      if (answer[post_id].answer == post_answer) {
+        User.find({ provider: req.user.provider, provider_id: req.user.id }, function(err, user) {
+          Score.find({ user_id: user._id }, function(err, score) {
+            if (score.length == 0) {
+              var score = new Score({ user_id : user._id, problem_id : post_id, score : answer[post_id].score });
+              score.save(function(err) {
+                if (err) { console.log(err); }
+              });
+            }
+          });
+          res.send("Valid answer.");
+        });
+      } else {
+        res.send("Invalid answer.");
+      }
+    } catch (e) {
+      res.send("Invalid data.");
+    }
+  } else {
+    res.redirect('../');
+  }
+});
+
+module.exports = router;
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   if (req.isAuthenticated()) {
-    res.render('dashboard', { title: 'CTF Dashboard', nickname: req.user.username, profile: JSON.stringify(req.user, null, 4) });
+    // âÒìöèÛãµÇåüçı
+    ////////////////////////////////////////////////////////////////
+    // MongoDBèâä˙âª
+    var Schema = mongoose.Schema;
+    var UserSchema = new Schema({
+      provider: { type: String, required: true },
+      provider_id: { type: String, required: true },
+      nickname: { type: String },
+      created: { type: Date, default: Date.now },
+      updated: { type: Date, default: Date.now }
+    });
+    var ScoreSchema = new Schema({
+      user_id: { type: String, required: true },
+      problem_id: { type: String, required: true },
+      score: { type: Number }
+    });
+
+    var mongodb_uri = process.env.MONGODB_URI || '';
+    mongoose.model('User', UserSchema);
+    mongoose.model('Score', ScoreSchema);
+    mongoose.Promise = global.Promise;
+    mongoose.connect(mongodb_uri);
+    var User = mongoose.model('User');
+    var Score = mongoose.model('Score');
+    
+    User.find({ provider: req.user.provider, provider_id: req.user.id }, function(err, user) {
+      Score.find({ user_id: user._id }, function(err, scores) {
+        res.render('dashboard', { title: 'CTF Dashboard', nickname: req.user.username, profile: JSON.stringify(req.user, null, 4), scores: scores });
+      }
+    }
   } else {
     res.redirect('../');
   }
