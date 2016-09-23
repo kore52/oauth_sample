8cab8cb75d7586d1935e0a8703be4b9d785f8b06
kore52 <lm312v21@gmail.com>
2016-09-23 16:28:44 +0900

    test

--- app.js
+++ app.js
@@ -70,8 +70,8 @@ var ScoreSchema = new Schema({
 });
 
 var mongodb_uri = process.env.MONGODB_URI || '';
-mongoose.model('User', UserSchema);
-mongoose.model('Score', ScoreSchema);
+//mongoose.model('User', UserSchema);
+//mongoose.model('Score', ScoreSchema);
 mongoose.Promise = global.Promise;
 mongoose.connect(mongodb_uri);
 var User = mongoose.model('User');
