a44a43a334fe17e0051e607e76f0b7fce115de27
kore52 <lm312v21@gmail.com>
2016-09-23 11:05:51 +0900

    test

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ var ids = {
     clientID : "e858816e383dc1a444fb",
     clientSecret : "8725ea28858065a8fc398ea5e0743e285bca1a6c",
     //callbackURL : "http://oauth-sample-001.herokuapp.com/auth/github/callback"
-    callbackURL : "http://localhost:3000/auth/github/callback"  }
+    callbackURL : process.env.OAUTH_GITHUB_CALLBACK_URI || "http://localhost:3000/auth/github/callback"  }
 }
 
 module.exports = ids
