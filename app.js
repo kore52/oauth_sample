ad3a50fc0402edf8aafc8aaa8340005de39703fa
kore52 <lm312v21@gmail.com>
2016-09-23 10:52:51 +0900

    test

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+var globalTunnel = require('global-tunnel');
+globalTunnel.initialize();
 
 var session = require('express-session');
 var partials = require('express-partials');
 
 // ページ遷移
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var dashboard = require('./routes/dashboard');
 //var register = require('./routes/register');
 var login = require('./routes/login');
 
@@ -25,9 +29,10 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
 app.use(partials());
 app.use(session({
   secret: 'the quick brown fox',
@@ -38,52 +43,110 @@ app.use(session({
   }
 }));
 
-
-/*
-var sessionCheck = function(req, res) {
-  if(req.session.user) {
+////////////////////////////////////////////////////////////////
+// セッションチェック
+var sessionCheck = function(req, res, next) {
+  if (req.session.user) {
     next();
   } else {
     res.redirect('/');
   }
 };
-*/
 
 ////////////////////////////////////////////////////////////////
+// MongoDB初期化
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+  provider: { type: String, required: true },
+  provider_id: { type: String, required: true },
+  nickname: { type: String },
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now }
+});
+var ScoreSchema = new Schema({
+  user_id: { type: String, required: true },
+  problem_id: { type: String, required: true },
+  score: { type: Number }
+});
+
+mongoose.model('User', UserSchema);
+mongoose.model('Score', ScoreSchema);
+mongoose.connect(MONGOLAB_URI);
+var User = mongoose.model('User');
+var Score = mongoose.model('Score');
+////////////////////////////////////////////////////////////////
 // ユーザーデータベース処理
+/*
 var NeDB = require('nedb')
 var db = {}
 db.users = new NeDB({
   filename: 'usersfile'
 });
 db.users.loadDatabase();
-
+*/
 
 ////////////////////////////////////////////////////////////////
-// OAuth処理
-var oauthconfig = require('./oauth.js');
+// 認証処理
+var authconfig = require('./auth.js');
 var passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+////////////////////////////////////////////////////////////////
+// ローカル認証
+var LocalStrategy = require('passport-local').Strategy;
+var crypto = require("crypto");
+/*
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    
+    // 入力されたパスワードからMD5ハッシュを作成
+    var md5 = crypto.createHash('md5');
+    md5.update(password);
+    var hash = md5.digest('hex');
+    
+    // 入力されたユーザー名・パスワードハッシュでDBを検索
+    db.users.find({name: username, password: hash}, function(err, user){
+      console.log('find', err, user);
+      if (err) {
+        console.log(err);
+        return done(err);
+      }
+      if (user.length == 0) {
+        console.log('User not found:', username);
+        return done(null, false);
+      }
+      
+      return done(null, user);
+    });
+  }
+));
+
+app.post('/auth/local',
+  passport.authenticate('local', { successRedirect: '/dashboard', failureRedirect: '/', failureFlash: 'Invalid username or password.' }));
+*/
 ////////////////////////////////////////////////////////////////
 // GitHubアカウントによるOAuth処理
 var GitHubStrategy = require('passport-github').Strategy;
 passport.use(new GitHubStrategy({
-    clientID: oauthconfig.github.clientID,
-    clientSecret: oauthconfig.github.clientSecret,
-    callbackURL: oauthconfig.github.callbackURL,
+    clientID: authconfig.github.clientID,
+    clientSecret: authconfig.github.clientSecret,
+    callbackURL: authconfig.github.callbackURL,
   },
   function(token, tokenSecret, profile, done) {
-    console.log('user-authentication');
-    db.users.find({name: profile.username, provider: profile.provider}, function(err, docs){
-      console.log('finding user');
-      if (docs === []) {
-        console.log('creating user');
-        db.users.insert({name: profile.username, provider: profile.provider});
+    User.find({provider: profile.provider, id: profile.id}, function(err, docs){
+      if (docs.length == 0) {
+        var user = new User({ provider: profile.provider, id: profile.id, nickname: profile.name });
+        user.save(function(err) {
+          if (err) { console.log(err); }
+        });
       }
+      if (err) {
+        return done(err);
+      }
+      return done(null, profile);
     });
-    return done(null, profile);
   }
 ));
 passport.serializeUser(function(user, done) {
@@ -95,17 +158,23 @@ passport.deserializeUser(function(user, done) {
 });
 
 app.get('/auth/github', passport.authenticate('github'));
-
 app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
   function (req, res) {
-    res.redirect('/users');
+    req.session.user = {name: req.body.username};
+    res.redirect('/dashboard');
   }
 );
 
 
 app.use('/', routes);
 app.use('/login', login);
-app.use('/users', users);
+app.use('/dashboard', sessionCheck, dashboard);
+
+
+app.get('/logout', function(req, res) {
+  delete req.session.user;
+  res.redirect('/');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- auth.js
+++ auth.js
@@ -2,8 +2,8 @@ var ids = {
   github : {
     clientID : "e858816e383dc1a444fb",
     clientSecret : "8725ea28858065a8fc398ea5e0743e285bca1a6c",
-    callbackURL : "http://oauth-sample-001.herokuapp.com/auth/github/callback"
-  }
+    //callbackURL : "http://oauth-sample-001.herokuapp.com/auth/github/callback"
+    callbackURL : "http://localhost:3000/auth/github/callback"  }
 }
 
 module.exports = ids
