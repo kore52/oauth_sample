7a97d6dbb314435336c9bdcf7be63840c11a4b63
kore52 <lm312v21@gmail.com>
2016-09-23 16:47:18 +0900

    test

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var flash = require('connect-flash');
