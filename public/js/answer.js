b68a8b04e6ae05193a0c732e0c3b7359404b305f
kore52 <lm312v21@gmail.com>
2016-09-26 14:36:47 +0900

    test

--- public/js/answer.js
+++ public/js/answer.js
@@ -6,8 +6,10 @@ $('#btn_problem_1').click(function() {
     dataType: 'json',
     contentType: 'application/json',
     data: JSON.stringify({ "problem_id" : "problem_1", "answer" : answer }),
-    success: function(data) {
-      alert("success");
+    success: function(result) {
+      if (result.status == "ok") {
+        
+      }
     },
     error: function(XHR, status, err) {
       alert("NG:" + status.status + err + XHR);
