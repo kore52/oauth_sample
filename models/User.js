faa056da23ea7113f945ec9f8bffe2b0ec7036b8
kore52 <lm312v21@gmail.com>
2016-09-28 17:25:08 +0900

    Commonize problem module

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ var UserSchema = new Schema({
   provider: { type: String, required: true },
   provider_id: { type: String, required: true },
   nickname: { type: String },
-  answered_problem: { type: String }
+  answered_problem: { type: String },
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now }
 });
