0cd097c2e8a1efe02a37e062822ba119e23f4368
kore52 <lm312v21@gmail.com>
2016-09-29 14:13:42 +0900

    test

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ var UserSchema = new Schema({
   provider: { type: String, required: true },
   provider_id: { type: String, required: true },
   nickname: { type: String },
-  answered_problem: { type: any },
+  answered_problem: { type: Schema.Types.Mixed },
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now }
 });
