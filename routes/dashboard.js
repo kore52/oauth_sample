e7b16cc6afd6bb4f052005289e660fd29920d16d
kore52 <lm312v21@gmail.com>
2016-10-05 12:59:30 +0900

    fix dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -15,11 +15,12 @@ router.get('/', function(req, res, next) {
         // ‰ñ“šó‹µ‚ğŒŸõ
         var User = model.User;
         var condition;
-        console.log("req.user:", req.user);
+        console.log("req.user=", req.user);
         if (req.user.provider == 'google' || req.user.provider == 'github')
           condition = { provider: req.user.provider, provider_id: req.user.id };
         else if (req.user.provider == 'twitter')
           condition = { provider: req.user.provider, provider_id: req.user.provider_id };
+        console.log("condition=", condition);
         User.findOne(condition, function(err, user) {
             if (err) throw err;
             if (user == null) throw "User not found.";
