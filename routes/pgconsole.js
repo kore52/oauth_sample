f89e37adeb659042a7a4675d02ced6872fdf48dc
kore52 <lm312v21@gmail.com>
2016-10-06 09:58:57 +0900

    fix pgconsole

--- routes/pgconsole.js
+++ routes/pgconsole.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('pgconsole');
+    res.render('pgconsole', { output: "" } );
 });
 
 router.post('/', function(req, res, next) {
