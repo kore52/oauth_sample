b2f9040d9b203f68dc24e01e1b8cfaef9cb2427b
kore52 <lm312v21@gmail.com>
2016-10-07 16:05:38 +0900

    webapp3

--- routes/problem/problem_public.js
+++ routes/problem/problem_public.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/webapp3-api', function(res, req, next) {
+  res.contentType("application/json");
+  res.end(JSON.stringify({
+    "apiversion": "1.0",
+    "status": "OK",
+    "flag": "PEEKING_YOUR_RESPONSE" }));
+});
+
+
 router.get('/:problemId', function(req, res, next) {
     var model = require('../../model');
 
@@ -78,14 +87,6 @@ router.post('/webapp2', function(req, res, next) {
         });
     });
 
-});
-
-router.get('/webapp3-api', function(res, req, next) {
-  res.contentType("application/json");
-  res.end(JSON.stringify({
-    "apiversion": "1.0",
-    "status": "OK",
-    "flag": "PEEKING_YOUR_RESPONSE" }));
 });
 
 module.exports = router;
