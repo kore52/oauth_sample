f270f9f7cc8c69bdb328c7f26ec12149e4fbd49e
kore52 <lm312v21@gmail.com>
2016-10-11 13:51:33 +0900

    webapp4

--- test.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-var username='alice';
-
-var buf = fs.readFileSync('./logfile');
-var match = new RegExp('.*'+ username +'.*', 'gm');
-
-var result = buf.toString().match(match);
-console.log(result);
